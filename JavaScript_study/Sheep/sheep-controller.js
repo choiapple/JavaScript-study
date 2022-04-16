3f39f043dbf8d779693b1a7d9b6e0405a9c7d82d
choiapple <79374648+choiapple@users.noreply.github.com>
2022-04-16 17:51:24 +0900

    Create sheep-controller.js

---
+++ JavaScript_study/Sheep/sheep-controller.js
@@ -0,0 +1,24 @@
+import{
+  Sheep
+} from "./sheep.js";
+export class SHeepController{
+  constructor(){
+
+  }
+  resize(stageWidth, stageHeight){
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+  }
+
+  loaded(){
+
+  }
+
+  addSheep(){
+
+  }
+
+  draw(ctx, t, dots){
+
+  }
+}
